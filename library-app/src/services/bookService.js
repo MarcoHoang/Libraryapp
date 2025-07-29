651f0e0b9c9ae3f82a4e77308058798a930c105c
MarcoHoang <hoang111uuu@gmail.com>
2025-07-29 10:02:34 +0700

    first

--- library-app/src/services/bookService.js
+++ library-app/src/services/bookService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/books";
+const API_URL = "http://localhost:3001";
 
 export const getAllBooks = (params = {}) => {
   const queryParams = {};
